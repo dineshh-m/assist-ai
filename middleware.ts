9dd3ad157b2eb0dc41070027409c4fbae7dc48b7
Dinesh882002 <98159300+Dinesh882002@users.noreply.github.com>
2024-03-27 11:31:20 +0530

    Fix rendering signup/signin pages when already loggedin

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,19 @@
-import { jwtVerify } from 'jose';
-import { cookies } from 'next/headers';
-import { NextResponse, NextRequest } from 'next/server';
-import { getAuthSecretKey } from './app/lib/util';
- 
+import { jwtVerify } from "jose";
+import { cookies } from "next/headers";
+import { NextResponse, NextRequest } from "next/server";
+import { getAuthSecretKey } from "./app/lib/util";
+
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
 
 export async function middleware(request: NextRequest) {
   // Get the auth_token header cookie
-  const authToken = cookies().get('auth_token');
+  const authToken = cookies().get("auth_token");
   // If url path start with chat check if the user is authenticated
-  if (request.nextUrl.pathname.startsWith('/chat')) {
+  const pathname = request.nextUrl.pathname;
+  if (pathname.startsWith("/chat")) {
     // verify jwt if already authenticated
     if (authToken && process.env.AUTH_SECRET) {
       try {
@@ -22,17 +23,32 @@ export async function middleware(request: NextRequest) {
         );
         const headers = new Headers(request.headers);
         // Custom header
-        headers.set('x-user-id', String(token.payload.userId));
+        headers.set("x-user-id", String(token.payload.userId));
         // proceed request processing if jwt is verified
         return NextResponse.next({
-          headers: headers
+          headers: headers,
         });
       } catch (error) {
         console.log(error);
-        throw new Error('Server error occured');
+        throw new Error("Server error occured");
       }
     }
     // Redirect to signin if user is not authenticated
     return NextResponse.redirect(new URL("/signin", request.url));
+  } else if (pathname.startsWith("/signin") || pathname.startsWith("/signup")) {
+    const signin = pathname.startsWith("/signin");
+    if (authToken && process.env.AUTH_SECRET) {
+      try {
+        jwtVerify(
+          authToken.value,
+          new TextEncoder().encode(getAuthSecretKey())
+        );
+        return NextResponse.redirect(new URL("/chat", request.url));
+      } catch (error) {
+        return NextResponse.redirect(
+          new URL(signin ? "/sigin" : "/signup", request.url)
+        );
+      }
+    }
   }
 }
